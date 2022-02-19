3b5cb43347059c098cf696d81e7398af0901fd28
kannan-santharam <kannan.santharam@freshworks.com>
2022-02-19 11:34:35 +0530

    added "mark as complete" feature

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
+let taskListSection = document.querySelector(".task-list-section ol");
+function checkForEmpty() {
+    if (taskListSection.querySelector("li")) {
+        document.querySelector(".task-list-section .show-task").style.display = "block";
+        document.querySelector(".task-list-section .no-task").style.display = "none";
+    } else {
+        document.querySelector(".task-list-section .show-task").style.display = "none";
+        document.querySelector(".task-list-section .no-task").style.display = "block";
+    }
+}
+
 function insertIntoList(taskName) {
-    let taskListSection = document.querySelector(".task-list-section ul");
+    // let taskListSection = document.querySelector(".task-list-section ol");
     let liElement = document.createElement("li");
     liElement.textContent = taskName;
-    taskListSection.append(liElement)
-
+    taskListSection.append(liElement);
+    checkForEmpty()
 }
 
 let addTaskBtn = document.querySelector(".addTask");
-addTaskBtn.addEventListener("click", function (e) {
+addTaskBtn.addEventListener("click", function () {
     let taskInput = document.querySelector(".todo-input").value;
     document.querySelector(".todo-input").value = ""
     console.log(taskInput)
     insertIntoList(taskInput);
-})
+});
+
+taskListSection.addEventListener("click", function (e) {
+    e.target.remove();
+    checkForEmpty()
+});
